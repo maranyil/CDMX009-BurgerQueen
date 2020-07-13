ce16e5b0fc2f4ff8379713d05535e3dfbb8b7dc5
Mara Mulato <maramulato@MacBook-Mara.local>
2020-07-13 16:45:10 -0500

    React-Router. Render conflict in progress

---
+++ my-app/src/pages/IncomingOrder.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+import Footer from '../components/Footer'
+
+function IncomingOrder() {
+    return(
+    <div>
+        <h1> Soy una orden agaga</h1>
+        <Footer />
+    </div> 
+    )
+}
+
+export default IncomingOrder
