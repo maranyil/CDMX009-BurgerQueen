02b52724f7183382c5437e614767572dbe14507c
Maranyil <mara.mulato@gmail.com>
2020-07-29 14:09:15 -0500

    login form not working yet

--- my-app/src/components/LoginForm/Forms.js
+++ my-app/src/components/LoginForm/Forms.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
+import 'firebase/auth'
+import firebase from 'firebase/app'
 import './Forms.css'
 
-function Forms() {
-    return(
-        <div className = "input-form">
+function Forms(props) {
+  const [email, setEmail] = useState ('');
+  const [password, setPassword] = useState ('');
+  const user = firebase.auth().currentUser;
+
+  const login = async() => {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+
+  const logout = async() => {
+    await firebase.auth().signOut();
+  }
+
+        return (
+        <div>
+          { !user &&
+          <div className = "input-form">
              <form className = "form">
-              <input className = 'inputf' type = 'email' placeholder = 'Email'></input> <br/>
-              <input className = 'inputf' type = 'password' placeholder = 'Password'></input> <br/>
-              <button className = 'btn' id = 'loginBtn'> Log In </button>
+              <input className = 'inputf' type = 'email' id = 'email' placeholder = 'Email'
+              onChange = { (ev)=> setEmail(ev.target.value) }></input> <br/>
+              <input className = 'inputf' type = 'password' id = 'password' placeholder = 'Password' 
+              onChange = { (ev)=> setPassword(ev.target.value) }></input> <br/>
+              <button className = 'btn' id = 'loginBtn' onClick = {login}> Login </button>
             </form>
-        </div>
-    )
+            </div>
+          }
+          { user &&
+      <button className = 'btn' id = 'logoutBtn' onClick = {logout}> Log Out</button>
+          }
+      </div> )
 }
 
 export default Forms
--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css'
 function Navbar() {
     return(
         <div className= "topbar">
-            <h1>Burguer Queen</h1>
+            <h1>Burger Queen</h1>
         </div>
     )
 }
