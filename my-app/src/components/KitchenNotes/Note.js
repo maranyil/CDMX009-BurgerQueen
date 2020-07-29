253d5ea49c1ac6388980c70b5f9a19b61a4328fe
keupa <kapirupia@gmail.com>
2020-07-28 22:04:19 -0500

    incoming order in progress, styles NEEDED

--- my-app/src/components/KitchenNotes/Note.js
+++ my-app/src/components/KitchenNotes/Note.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { db, auth } from '../../firebase'
+import { v4 as uuidv4 } from 'uuid'
 import './Note.css'
 
 function Note(){
+
+    const [ incoming, setIncoming ] = useState([])
+
+    const getOrders = () =>{
+        db
+        .collection('orders')
+        //.where("status", "==", 'in progress')
+        .orderBy('date', 'desc')
+        .onSnapshot((querySnapshot) => {
+            const docs = []
+            querySnapshot.forEach((doc) => {
+                docs.push({...doc.data(), id: doc.id})
+            })
+            setIncoming(docs)
+        })
+    }
+
+    useEffect(() => {
+        getOrders()
+      }, []);
+
+    const updateOrder = () => {
+        alert('un meserx será notificado :)')
+    }
+
     return(
         <div className="note-container">
-            soy una nota 
+            {incoming.map(order => {
+                return (
+                    <div className="notes">
+                        <h2 className="table"> Mesa {order.table}</h2>
+                        <p> Meserx: {order.waiter}</p>
+                        {order.items.map(product => {
+                            return (
+                                <div key={uuidv4()} >
+                                    <p>{product.item}</p>
+                                    <p>{product.quantity}</p>
+                                </div>
+                            )
+                        })}
+                        <button className="green" onClick={updateOrder}>Orden lista!</button>
+                    </div>
+                )
+            })}
         </div>
     )
 }
--- my-app/src/components/KitchenNotes/index.js
+++ my-app/src/components/KitchenNotes/index.js
@@ -1,18 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import Note from './Note'
-import { db, auth } from '../../firebase'
 
 function KitchenNotes () {
-
-    const [ note, setNote ] = useState([])
-
-    useEffect(() => {
-      }, []);
-
     return(
         <>
-            <Note />
-            <Note />
             <Note />
         </>
     )
