5b092bd01c5c2d7e0516b3d422447759f480bc56
keupa <kapirupia@gmail.com>
2020-08-03 10:00:58 -0500

    dettach listener from component

--- my-app/src/components/KitchenNotes/Note.js
+++ my-app/src/components/KitchenNotes/Note.js
@@ -8,32 +8,48 @@ function Note(){
     const [ incoming, setIncoming ] = useState([])
 
     const getOrders = () =>{
-        db
+        return db
         .collection('orders')
-        //.where("status", "==", 'in progress')
+        .where("status", "==", "in progress")
         .orderBy('date', 'desc')
         .onSnapshot((querySnapshot) => {
             const docs = []
             querySnapshot.forEach((doc) => {
                 docs.push({...doc.data(), id: doc.id})
             })
+            console.log(docs)
             setIncoming(docs)
         })
-    }
+    } 
 
     useEffect(() => {
-        getOrders()
+        let unsubscribe = getOrders()
+
+        return () => {
+            unsubscribe()
+        }
       }, []);
 
-    const updateOrder = () => {
-        alert('un meserx será notificado :)')
+    const updateOrder = (id) => {
+        db    
+        .collection('orders')
+        .doc(id)
+        .update({
+            status: "done"
+        })
+        .then(() => {
+            alert('un meserx será notificado :)')
+        })
+        .catch(() =>{
+            alert('Ocurrió un error, intentalo de nuevo en un momento')
+        })   
     }
 
     return(
         <div className="note-container">
             {incoming.map(order => {
                 return (
-                    <div className="notes">
+                    <div className="notes" key={order.id}>
                         <h2 className="table"> Mesa {order.table}</h2>
                         <p> Meserx: {order.waiter}</p>
                         {order.items.map(product => {
@@ -44,7 +60,7 @@ function Note(){
                                 </div>
                             )
                         })}
-                        <button className="green" onClick={updateOrder}>Orden lista!</button>
+                        <button className="green" onClick={() => {console.log(order); updateOrder(order.id)}}>Orden lista!</button>
                     </div>
                 )
             })}
--- my-app/src/components/KitchenNotes/index.js
+++ my-app/src/components/KitchenNotes/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Note from './Note'
 
 function KitchenNotes () {
