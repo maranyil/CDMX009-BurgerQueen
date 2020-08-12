551d9a24724889196b551f3896e5dae39dec74cb
keupa <kapirupia@gmail.com>
2020-08-12 13:54:18 -0500

    created build for netlify

---
+++ build/precache-manifest.5f85d8cbce6d405260944d39fcc955a0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f78e27b2538e3d269fd1adcc07b867",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8212fef7ea35c608479",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "f7c3ce5c0aa793d40b50",
+    "url": "/static/css/main.36f15960.chunk.css"
+  },
+  {
+    "revision": "e8212fef7ea35c608479",
+    "url": "/static/js/2.29bfcd43.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/static/js/2.29bfcd43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c3ce5c0aa793d40b50",
+    "url": "/static/js/main.b5c3fb02.chunk.js"
+  },
+  {
+    "revision": "91dbc765750696e66f95",
+    "url": "/static/js/runtime-main.19e996be.js"
+  },
+  {
+    "revision": "431d6a8b94b154cad61dffadc35f57bf",
+    "url": "/static/media/LogoBQ.431d6a8b.svg"
+  },
+  {
+    "revision": "664bd14593e629b1e9d4c400f3549623",
+    "url": "/static/media/american-coffee.664bd145.webp"
+  },
+  {
+    "revision": "b27ee13f5e485c8f8930a3ad84b6d949",
+    "url": "/static/media/coffee-milk.b27ee13f.webp"
+  },
+  {
+    "revision": "cf6c9ef27b151ddf44f0af1e067456b5",
+    "url": "/static/media/double-hamb.cf6c9ef2.webp"
+  },
+  {
+    "revision": "e727c2cf0817f4d0ea8f638aa20161ad",
+    "url": "/static/media/fries.e727c2cf.webp"
+  },
+  {
+    "revision": "8bba1fa5d2ffef5c8a0e18c89b7a0aa3",
+    "url": "/static/media/juice.8bba1fa5.webp"
+  },
+  {
+    "revision": "961342c08a3e2f4b501a877a76834596",
+    "url": "/static/media/logout.961342c0.svg"
+  },
+  {
+    "revision": "86f422f84d3c744b089c46c0e54de3f7",
+    "url": "/static/media/onion-rings.86f422f8.webp"
+  },
+  {
+    "revision": "dfde55304b41352107638543b1ed02f6",
+    "url": "/static/media/sandwich.dfde5530.webp"
+  },
+  {
+    "revision": "0b0091c735a00f6b6083720ff52d4c22",
+    "url": "/static/media/simple-hamb.0b0091c7.webp"
+  },
+  {
+    "revision": "fb0b500699329733372ad0f652413a5c",
+    "url": "/static/media/soda.fb0b5006.webp"
+  },
+  {
+    "revision": "5a226e36b139f8e3415d5de7358db0ed",
+    "url": "/static/media/trash.5a226e36.svg"
+  },
+  {
+    "revision": "374b02287cdbaf49b396c4989dd3dde8",
+    "url": "/static/media/wata.374b0228.webp"
+  }
+]);
